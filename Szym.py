2698d62c7b67c8c38955d69ed776df3cbfafcee6
BlueBirdYT <46397900+BlueBirdYT@users.noreply.github.com>
2019-02-05 19:23:13 +0600

    Update Szym.py

--- Szym.py
+++ Szym.py
@@ -8,6 +8,7 @@ import random
 import json
 import aiohttp
 import datetime
+import os
 
 Forbidden= discord.Embed(title="Permission Denied", description="1) Please check whether you have permission to perform this action or not. \n2) Please check whether my role has permission to perform this action in this channel or not. \n3) Please check my role position.", color=0x00ff00)
 client = commands.Bot(description="prefix is R", command_prefix=commands.when_mentioned_or("R" ), pm_help = True)
